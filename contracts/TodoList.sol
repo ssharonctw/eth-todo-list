c789b6728fc3eb824020d6b28816e24951bf116e
ssharonctw <ssharonctw@hotmail.com>
2022-03-14 00:14:37 +0800

    Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract TodoList{
+  //declare a state variable
+  //State variables are written to Blockchain
+  //it represents the state of this smart contract
+  uint public taskCount = 0;
+
+}
